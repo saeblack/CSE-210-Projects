ff5f7249c08eeac3b4a2d9f8216ca6339c278b24
saeblack <bla22047@byui.edu>
2024-05-15 00:14:43 -0600

    finished loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        PlayGuessingGame();
+    }
+
+    static void PlayGuessingGame()
+    {
+        Random rng = new Random();
+        int targetNumber = rng.Next(1, 101);
+
+        int userGuess = -1;
+
+        while (userGuess != targetNumber)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+
+            if (targetNumber > userGuess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (targetNumber < userGuess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
     }
 }
+
