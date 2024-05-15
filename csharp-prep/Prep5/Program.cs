c0c5b84ac87cd853dc0a903c1eb3580a06a00b24
saeblack <bla22047@byui.edu>
2024-05-15 00:34:13 -0600

    finished fav number

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        ShowWelcomeMessage();
+        string userName = GetUserName();
+        int favoriteNumber = GetFavoriteNumber();
+        int squaredNumber = CalculateSquare(favoriteNumber);
+        ShowResult(userName, squaredNumber);
+    }
+
+    static void ShowWelcomeMessage()
+    {
+        Console.WriteLine("Welcome to the program!");
+    }
+
+    static string GetUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+        return name;
+    }
+
+    static int GetFavoriteNumber()
+    {
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+        return number;
+    }
+
+    static int CalculateSquare(int number)
+    {
+        int square = number * number;
+        return square;
+    }
+
+    static void ShowResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
